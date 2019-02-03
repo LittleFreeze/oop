e87c62d344513828a1e7f6e5b6bf4d8858281c65
Evan Von Duhn <ecv8@zips.uakron.edu>
2019-02-03 03:11:21 -0500

    Update

---
+++ War Game/deck.hpp
@@ -0,0 +1,39 @@
+#ifndef DECK_HPP_INCLUDED
+#define DECK_HPP_INCLUDED
+
+enum Suit
+{
+    Hearts,
+    Clubs,
+    Spades,
+    Diamonds
+};
+
+enum Rank
+{
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+};
+
+class Deck
+{
+    private:
+    struct Card
+    {
+        /* data */
+    };
+    
+}
+
+#endif
