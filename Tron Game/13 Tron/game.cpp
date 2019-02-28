48b8f2a6f3581812ece199a7185e0dd99f8ba711
Evan Von Duhn <ecv8@zips.uakron.edu>
2019-02-28 12:59:53 -0500

    Update game.cpp

--- Tron Game/13 Tron/game.cpp
+++ Tron Game/13 Tron/game.cpp
@@ -21,7 +21,7 @@ void game::setupGame()
 
 	Sprite sprite;
 	RenderTexture t;
-	t.create(W, H);
+	t.create(b->getBoardWidth(), b->getBoardHeight());
 	t.setSmooth(true);
 	sprite.setTexture(t.getTexture());
     t.clear();  t.draw(sBackground);
@@ -40,29 +40,29 @@ void game::startGame()
                 window.close();
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Left)) if (p1.dir!=2) p1.dir=1;
-	    if (Keyboard::isKeyPressed(Keyboard::Right)) if (p1.dir!=1)  p1.dir=2;
-	    if (Keyboard::isKeyPressed(Keyboard::Up)) if (p1.dir!=0) p1.dir=3;
-		if (Keyboard::isKeyPressed(Keyboard::Down)) if (p1.dir!=3) p1.dir=0;
+		if (Keyboard::isKeyPressed(Keyboard::Left)) if (p1->dir!=2) p1->dir=1;
+	    if (Keyboard::isKeyPressed(Keyboard::Right)) if (p1->dir!=1)  p1->dir=2;
+	    if (Keyboard::isKeyPressed(Keyboard::Up)) if (p1->dir!=0) p1->dir=3;
+		if (Keyboard::isKeyPressed(Keyboard::Down)) if (p1->dir!=3) p1->dir=0;
 
-		if (Keyboard::isKeyPressed(Keyboard::A)) if (p2.dir!=2) p2.dir=1;
-	    if (Keyboard::isKeyPressed(Keyboard::D)) if (p2.dir!=1)  p2.dir=2;
-	    if (Keyboard::isKeyPressed(Keyboard::W)) if (p2.dir!=0) p2.dir=3;
-		if (Keyboard::isKeyPressed(Keyboard::S)) if (p2.dir!=3) p2.dir=0;
+		if (Keyboard::isKeyPressed(Keyboard::A)) if (p2->dir!=2) p2->dir=1;
+	    if (Keyboard::isKeyPressed(Keyboard::D)) if (p2->dir!=1)  p2->dir=2;
+	    if (Keyboard::isKeyPressed(Keyboard::W)) if (p2->dir!=0) p2->dir=3;
+		if (Keyboard::isKeyPressed(Keyboard::S)) if (p2->dir!=3) p2->dir=0;
 
 		if (!Game)	continue;
 
 		for(int i=0;i<speed;i++)
 		{
 			p1.tick(); p2.tick();
-			if (field[p1.x][p1.y]==1) Game=0; 
-			if (field[p2.x][p2.y]==1) Game=0;
-			field[p1.x][p1.y]=1; 
-			field[p2.x][p2.y]=1;
+			if (field[p1->x][p1->y]==1) Game=0; 
+			if (field[p2->x][p2->y]==1) Game=0;
+			field[p1->x][p1->y]=1; 
+			field[p2->x][p2->y]=1;
 	
 			CircleShape c(3);
-			c.setPosition(p1.x,p1.y); c.setFillColor(p1.color);	t.draw(c);
-			c.setPosition(p2.x,p2.y); c.setFillColor(p2.color);	t.draw(c);
+			c.setPosition(p1->x,p1->y); c.setFillColor(p1->color);	t.draw(c);
+			c.setPosition(p2->x,p2->y); c.setFillColor(p2->color);	t.draw(c);
 			t.display();	
 		}
 
